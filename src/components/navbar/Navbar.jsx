5b570dbfaf637ccd844571539f23c171c1853532
Abbosbek <abbosbekqodirov2501@gmail.com>
2024-05-20 17:45:54 +0500

    dd

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <div className="navbar">
         <div className="container">
           <div className="location">
-            <img src="./imgs/icons/mark.svg" alt="" />
+            <img src="/imgs/icons/mark.svg" alt="" />
             Toshkent
           </div>
           <div className="contact">
@@ -18,7 +18,7 @@ function Navbar() {
               <option value="">Uzb</option>
             </select>
             <Link>
-              <img src="./imgs/icons/phone.svg" alt="" />
+              <img src="/imgs/icons/phone.svg" alt="" />
               +998999999999
             </Link>
           </div>
@@ -26,21 +26,23 @@ function Navbar() {
       </div>
       <div className="nav_bar">
         <div className="container">
-          <h2>Eshik deraza</h2>
+          <h2>
+            <Link to={"/"}>Eshik deraza</Link>
+          </h2>
           <form action="">
             <input placeholder="Nomi va turi bo’yicha qidiruv" type="text" />
-            <img src="./imgs/icons/search.svg" alt="" />
+            <img src="/imgs/icons/search.svg" alt="" />
           </form>
           <div className="user_info">
             <div>
-              <img src="./imgs/icons/cart.svg" alt="" />
+              <img src="/imgs/icons/cart.svg" alt="" />
               <span>2</span>
             </div>
             <div>
-              <img src="./imgs/icons/like.svg" alt="" />
+              <img src="/imgs/icons/like.svg" alt="" />
             </div>
             <div>
-              <img src="./imgs/man.png" alt="" />
+              <img src="/imgs/man.png" alt="" />
             </div>
           </div>
         </div>
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Filter from '../../components/filter/Filter'
+import "./Product.css"
 
 function Product() {
+    useEffect(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }, []);
   return (
     <div className="one_product ">
       <div className="container">
@@ -13,47 +20,59 @@ function Product() {
               <img src="/imgs/photos/mainImg1.png" alt="" />
             </div>
             <div className="main_imgs">
-              <img src="" alt="" />
-              <img src="" alt="" />
-              <img src="" alt="" />
-              <img src="" alt="" />
-              <img src="" alt="" />
+              <img src="/imgs/photos/infoImg1.png" alt="" />
+              <img src="/imgs/photos/infoImg2.png" alt="" />
+              <img src="/imgs/photos/infoImg3.png" alt="" />
+              <img src="/imgs/photos/infoImg4.png" alt="" />
+              <img src="/imgs/photos/infoImg5.png" alt="" />
             </div>
           </div>
           <div className="about_info">
-            <div>
+            <div className="about_title">
               <h1>MDF Eshiklar</h1>
               <p>⭐⭐⭐⭐ 40 komment</p>
+              <div className="about_like">
+                <img src="/imgs/icons/infos_like.svg" alt="" />
+              </div>
+            </div>
+            <div>
               <h1>1 200 000 so‘m</h1>
             </div>
 
             <div>
               <p>Rang</p>
               <div className="colors">
-                <span></span>
-                <span></span>
+                <span style={{ backgroundColor: "#5D412A" }}></span>
+                <span style={{ backgroundColor: "#E3B90F" }}></span>
                 <span></span>
               </div>
             </div>
 
             <div>
               <p>O‘lchamlar</p>
-              <span>120x200 sm</span>
-              <span>120x200 sm</span>
-              <span>120x200 sm</span>
-              <span>120x200 sm</span>
-              <span>120x200 sm</span>
-              <span>120x200 sm</span>
+              <div className="sizes">
+                <span className="active">120x200 sm</span>
+                <span>120x200 sm</span>
+                <span>120x200 sm</span>
+                <span>120x200 sm</span>
+                <span>120x200 sm</span>
+                <span>120x200 sm</span>
+              </div>
             </div>
 
             <div>
               <p>Brendlar</p>
-              <span>Akfa</span>
-              <span>MDF</span>
-              <span>Rasulov</span>
+              <div className="brands">
+                <span>Akfa</span>
+                <span className="active">MDF</span>
+                <span>Rasulov</span>
+              </div>
             </div>
             <div className="about_btns">
-              <button>Savatga qo‘shish</button>
+              <button>
+                {" "}
+                <img src="/imgs/icons/infocart.svg" alt="" /> Savatga qo‘shish
+              </button>
               <button>Sotib olish</button>
             </div>
           </div>
