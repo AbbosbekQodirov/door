4421ca4b58c3ab827e6c63e06dda52b51535a14d
Abbosbek <abbosbekqodirov2501@gmail.com>
2024-05-22 17:47:37 +0500

    dd

--- src/pages/basket/Basket.jsx
+++ src/pages/basket/Basket.jsx
@@ -3,19 +3,61 @@ import Filter from "../../components/filter/Filter";
 import Title from "../../components/title/Title";
 import Product from "../../components/product/Product";
 import Customers from "../../components/customers/Customers";
+import "./Basket.css"
 
 function Basket() {
   return (
     <div>
-      <div className="one_product ">
+      <div>
         <div className="container">
           <Filter />
           <p>Bosh sahifa • Savatcha</p>
           <Title title={"Savatcha"} />
-          <div className="BusketList">
-            <h1>
-              Hozircha Dasturchi boshqa ish qilyapti🫡😶‍🌫️
-            </h1>
+          <div className="busket">
+            <div className="busket_list">
+              <div className="allSelect">
+                <input type="checkbox" id="allItem" />
+                <label htmlFor="allItem">Barchasini belgilash</label>
+              </div>
+             {[1,2,3].map(()=>{
+              return (
+                <div className="busketItem">
+                  <input type="checkbox" />
+                  <div className="busketItemImg">
+                    <img src="/imgs/photos/busketProduct1.png" alt="" />
+                  </div>
+                  <div className="doorInfo">
+                    <h3>MDF Eshiklar</h3>
+                    <p>
+                      Rangi: &nbsp; &nbsp; <b> Jigarrang</b>
+                    </p>
+                    <p>
+                      O‘lchami: &nbsp; &nbsp; <b> 120x200 sm</b>
+                    </p>
+                    <p>
+                      Brend: &nbsp; &nbsp; <b> MDF</b>
+                    </p>
+                  </div>
+                  <div className="itemCount">
+                    <span className="countMinus">-</span>
+                    <span>1</span>
+                    <span className="countPlus">+</span>
+                  </div>
+                  <div className="nowPrice">
+                    <span>1 200 000 so‘m</span>
+                    <h3>1 000 000 so‘m</h3>
+                  </div>
+                </div>
+              );
+             })}
+            </div>
+            <div className="busketPrices">
+              <p>Savatdagi mahsulotlar: 3</p>
+              <p>
+                Jami miqdor: <b>3 000 000 so‘m</b>
+              </p>
+              <button>Rasmiylashitirish</button>
+            </div>
           </div>
           <Title title="Tavsiyalar" />
           <div className="products">
